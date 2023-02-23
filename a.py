73b3fe2af64190b80b5c4242f6b0d34ecc6e65de
futureplayer <mirryne@gmail.com>
2023-02-23 10:52:28 +0900

    first commit

---
+++ a.py
@@ -0,0 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
+
+requests.get('https://search.naver.com/search.naver?display=15&f=&filetype=0&page=2&query=%EB%A6%AC%EC%A5%AC%EB%9E%80%ED%9E%90%EB%9F%AC&research_url=&sm=tab_pge&start=1&where=web')
+requests.get('https://search.naver.com/search.naver?display=15&f=&filetype=0&page=3&query=%EB%A6%AC%EC%A5%AC%EB%9E%80%ED%9E%90%EB%9F%AC&research_url=&sm=tab_pge&start=16&where=web')
---
+++ app.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://instagram.com')
+
+time.sleep(5)
+e = driver.find_element(By.NAME, 'username')
+e.send_keys('test.test8850')
+
+time.sleep(5)
+e = driver.find_element(By.NAME,'password')
+e.send_keys('link2020')
+
+time.sleep(2)
+e.send_keys(Keys.ENTER)
